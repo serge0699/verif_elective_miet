1bca0aac50a30b1cefd535f81d76d7ce65b49418
vladimirkuzia <gameplan062@gmail.com>
2024-06-05 11:43:27 +0300

    pravki dz 2

--- lesson_2/tasks/05_router/testbench.sv
+++ lesson_2/tasks/05_router/testbench.sv
@@ -8,7 +8,7 @@ module testbench;
     logic  [3:0][1:0] sel;
     logic       [3:0] in;
     logic       [3:0] out;
-    logic       [3:0] out_ref;
+    bit         [3:0] out_ref;
     router DUT(
         .clk     ( clk     ),
         .aresetn ( aresetn ),
@@ -48,11 +48,11 @@ module testbench;
         wait(aresetn);
         repeat (check) begin
             @(posedge clk);
-            in[3:0] = $urandom_range(0,15);
+            in[3:0]     = $urandom_range(0,15);
             sel[0][1:0] = $urandom_range(0,3);
             sel[1][1:0] = $urandom_range(0,3);
             sel[2][1:0] = $urandom_range(0,3);
-            sel[3][1:0] = $urandom_range(0,3);
+            sel[3][1:0] = $urandom_range(0,3);#(CLK_PERIOD);
             check = check - 1;
             if(check == 0) flag = 1;
             if(flag) $stop();
@@ -66,6 +66,7 @@ module testbench;
         logic       [3:0] out;
     } packet;
 
+    mailbox#(packet) mon2chk = new();
 
     task put_pkt();
         packet pkt;
@@ -79,9 +80,6 @@ module testbench;
             end
     endtask
 
-    mailbox#(packet) mon2chk = new();
-
-
     // TODO:
     // Выполните проверку выходных сигналов
     task check();
@@ -89,19 +87,23 @@ module testbench;
         wait(aresetn);
         forever begin
             mon2chk.get(pkt_cur);
-            out_ref = 0;
             // Пишите здесь
+            out_ref = 0;
+            // $display("%t check  = ",$time);
+            // $display(" in  = %d ",pkt_cur.in[3:0]);
             for(int i = 0; i <= 3; i++ ) begin
-                out_ref[pkt_cur.sel[i][1:0]] = pkt_cur.in[i];
-                if(pkt_cur.sel[0][1:0] == pkt_cur.sel[1][1:0] || pkt_cur.sel[0][1:0] == pkt_cur.sel[2][1:0] || pkt_cur.sel[0][1:0] == pkt_cur.sel[3][1:0]) begin
-                    out_ref[pkt_cur.sel[0][1:0]] = pkt_cur.in[0];
-                end else if(pkt_cur.sel[1][1:0] == pkt_cur.sel[2][1:0] || pkt_cur.sel[1][1:0] == pkt_cur.sel[3][1:0])begin 
-                    out_ref[pkt_cur.sel[1][1:0]] = pkt_cur.in[1];
-                end else if(pkt_cur.sel[2][1:0] == pkt_cur.sel[3][1:0]) begin
-                    out_ref[pkt_cur.sel[2][1:0]] = pkt_cur.in[2];
-                end
-                if(out_ref !== out) $error("Router ERROR: ", " out:" , out , " out_ref:" , out_ref , " in:" , pkt_cur.in);
+                out_ref[pkt_cur.sel[i] ] = pkt_cur.in[i];
+            // $display(" out_ref  = %d", out_ref[pkt_cur.sel[i]], " out__sel = %d ", pkt_cur.sel[i], " pkt_in = %d " , pkt_cur.in[i]);
             end
+            // $display(" out_ref  = %d", out_ref);
+            if(pkt_cur.sel[0][1:0] == pkt_cur.sel[1][1:0] || pkt_cur.sel[0][1:0] == pkt_cur.sel[2][1:0] || pkt_cur.sel[0][1:0] == pkt_cur.sel[3][1:0]) begin
+                out_ref[pkt_cur.sel[0][1:0]] = pkt_cur.in[0];
+            end else if(pkt_cur.sel[1][1:0] == pkt_cur.sel[2][1:0] || pkt_cur.sel[1][1:0] == pkt_cur.sel[3][1:0])begin 
+                out_ref[pkt_cur.sel[1][1:0]] = pkt_cur.in[1];
+            end else if(pkt_cur.sel[2][1:0] == pkt_cur.sel[3][1:0]) begin
+                out_ref[pkt_cur.sel[2][1:0]] = pkt_cur.in[2];
+            end
+            if(out_ref !== out) $error("Router ERROR: ", " out:" , out , " out_ref:" , out_ref , " in:" , pkt_cur.in);
         end
     endtask
 
