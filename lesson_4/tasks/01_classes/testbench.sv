54ac5f05642c8ffe237cc5c938504b9c10d5582c
Alexander <alexandrinnikov@gmail.com>
2024-06-06 16:18:50 +0300

    write tb (before right checker)

--- lesson_4/tasks/01_classes/testbench.sv
+++ lesson_4/tasks/01_classes/testbench.sv
@@ -14,8 +14,15 @@ module testbench;
     //   4) сумма 'a' и 'b' больше 100
 
     class my_class_1;
+        rand bit [7:0] a;
+        rand bit [7:0] b;
 
-
+        constraint constr {
+            b > 0;
+            a % b == 0;
+            a > b;
+            a + b > 100;
+        }
     endclass
 
 
@@ -32,8 +39,14 @@ module testbench;
 
 
     class my_class_2;
+        rand bit [7:0] a;
+        rand bit [7:0] b;
 
-
+        constraint constr {
+            a == 0 -> b == 100;
+            a < 100;
+            b > 50;
+        }
     endclass
 
 
@@ -49,8 +62,17 @@ module testbench;
     //   4) если индекс элемента четный - элемент тоже четный
 
     class my_class_3;
-
-
+        rand int data[];
+        constraint constr {
+            data.size() % 2 == 0;
+            data.size() < 10;
+            foreach (data[i]) {
+                data[i] < 200;
+                if(i % 2 == 0) {
+                    data[i] % 2 == 0;
+                }
+            }
+        }
     endclass
 
 
@@ -66,8 +88,20 @@ module testbench;
     //      иначе каждый элемент 'data' уникален
 
     class my_class_4;
+        rand bit [1:0] size;
+        rand bit [7:0] data [];
 
+        constraint constr {
+            data.size() == size;
+            if(size == 3) {
+                foreach (data[i]) {
+                    data[i] == 0;
+                }
+            } else {
+                unique {data};
+            }
 
+        }
     endclass
 
 
@@ -85,7 +119,16 @@ module testbench;
     //   3) если 'req' и 'we' равны 1, то 'addr' меньше 128
 
     class my_class_5;
+    rand bit [7:0] data;
+    rand bit [7:0] addr;
+    rand bit req;
+    rand bit we;
 
+    constraint constr {
+        addr[1:0] == 2'b00;
+        req -> data inside {[100:200]};
+        (req && we) -> addr < 128;
+    }
 
     endclass
 
@@ -105,8 +148,36 @@ module testbench;
     //      чаще, чем раз в 4 значения
 
     class my_class_6;
+        rand bit [31:0] tdata  [];
+        rand bit        tid;
+        rand bit        tlast [];
 
+        constraint constr {
+            tdata.size() == tlast.size();
+            tdata.size() < 33;
+            tlast.size() < 33;
+            tdata.size() % 8 == 0;
+            tlast.size() % 8 == 0;
 
+            // (tlast.find_index()).size() <= tdata.size() / 4;
+            foreach(tlast[i]) {
+                if(tlast[i] == 1 && ~(i inside {tlast.size()-1, tlast.size()-2, tlast.size()-3})) {
+                    tlast[i+1] == 0;
+                    tlast[i+2] == 0;
+                    tlast[i+3] == 0;
+                }
+                else if(tlast[i] == 1 && i == tlast.size()-3 && {tlast[i-3], tlast[i-2], tlast[i-1]} == 3'b0) {
+                    tlast[i+1] == 0;
+                    tlast[i+2] == 0;
+                }
+                else if(tlast[i] == 1 && i == tlast.size()-2 && {tlast[i-3], tlast[i-2], tlast[i-1]} == 3'b0) {
+                    tlast[i+1] == 0;
+                }
+                // else if(tlast[i] == 1 && i == tlast.size()-1 && {tlast[i-3], tlast[i-2], tlast[i-1]} == 3'b0) {
+
+                // }
+            }
+        }
     endclass
 
     `include "checker.svh"
