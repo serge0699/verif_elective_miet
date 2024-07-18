b715aad5b6cd6e41b4b94941f693375fe53637a3
Crabikk <artem.visotskiyy@gmail.com>
2024-07-19 00:22:58 +0300

    Домашнее задание 4

--- lesson_6/tasks/03_sum/testbench.sv
+++ lesson_6/tasks/03_sum/testbench.sv
@@ -32,7 +32,110 @@ module testbench;
         // TODO:
         // Добавьте недостающие входные воздействия здесь.
         // ...
-        
+        $monitor("At time = %0t: a = %0d, b = %0d", $time, a, b);
+        for(int i=0;i<71;i=i+1) begin
+        @(posedge clk);
+            if(i%2==0) begin // a: ot 100,101,102,103... do 170
+            a <= i+100;
+            end
+            if(i%2==1 && i < 49) begin // a: ot 2,4,6,8... do 48
+            a <= i+1;
+            end
+            if(i%2==1 && i > 48) begin // a: ot 49,47,45,43... do 29
+            a <= 98 - i;
+            end
+            if(i%2==1) begin // b: ot 173,175,177,179,... do 241
+            b <= i+172;
+            end
+            if(i%2==0 && i <14) begin // b: ot 50,52,54,56,58,60,62
+            b <= i+50;
+            end
+            if(i%2==0 && i >13 && i <27) begin // b: ot 51,53,55,57,59,63
+            b <= i+37;
+            end
+            if(i%2==0 && i >27 &&  i<41) begin // b: ot 215,
+            b <= i+215;
+            end
+            
+        end
+        for(int i=172;i<256;i=i+1) begin
+        @(posedge clk);
+            b <= 0;
+            a <= i;
+        end
+
+        for(int i=0;i<108;i=i+1) begin
+        @(posedge clk);
+            b <= i+64;
+            a <= 255;
+        end
+
+        @(posedge clk);
+            a <= 1;
+            b <= 1;
+        @(posedge clk);
+            a <= 9;
+            b <= 1;
+        @(posedge clk);
+            a <= 0;
+            b <= 1;
+        @(posedge clk);
+            a <= 9;
+            b <= 0;
+        @(posedge clk);
+            a <= 0;
+            b <= 0;
+        @(posedge clk);
+            a <= 9;
+            b <= 255;
+        @(posedge clk);
+            a <= 180;
+            b <= 255;
+        @(posedge clk);
+            a <= 255;
+            b <= 255;
+        @(posedge clk);
+            a <= 3;
+        @(posedge clk);
+            a <= 5;
+        @(posedge clk);
+            a <= 7;
+        @(posedge clk);
+            a <= 9;
+        @(posedge clk);
+            a <= 11;
+        @(posedge clk);
+            a <= 13;
+        @(posedge clk);
+            a <= 15;
+        @(posedge clk);
+            a <= 17;
+        @(posedge clk);
+            a <= 19;
+        @(posedge clk);
+            a <= 21;
+        @(posedge clk);
+            a <= 23;
+        @(posedge clk);
+            a <= 25;
+        @(posedge clk);
+            a <= 27;
+
+        // Вывод результатов покрытия в консоль
+        $display("Результаты покрытия для sum_cg:");
+        $display("Покрытие a_i_cp:  %g%%", cg.a_i_cp.get_coverage());
+        $display("Покрытие a_in_cp: %g%%", cg.a_in_cp.get_coverage());
+        $display("Покрытие b_i_cp:  %g%%", cg.b_i_cp.get_coverage());
+        $display("Покрытие a_s_cp:  %g%%", cg.a_s_cp.get_coverage());
+        $display("Покрытие b_s_cp:  %g%%", cg.b_s_cp.get_coverage());
+        $display("Покрытие cross_1: %g%%", cg.cross_1.get_coverage());
+        $display("Покрытие cross_2: %g%%", cg.cross_2.get_coverage());
+        $display("Покрытие cross_3: %g%%", cg.cross_3.get_coverage());
+        $display("Покрытие cross_4: %g%%", sum_cg.cross_4.get_coverage());
+        $display("Покрытие sum:  %g%%", cg.get_inst_coverage());
+
+
+    
         @(posedge clk);
         ->> user_done;
     end
